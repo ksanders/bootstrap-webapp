3d22f3d752ea4e8fe899cda47b870b0ee5f2ef4a
Kieran Sanders <kieran@obaworld.net>
2018-03-03 00:33:19 +0000

    bootstrap

--- public/index.php
+++ public/index.php
@@ -8,7 +8,13 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque rhoncus metus a consectetur. Donec id ligula nulla. Vivamus mattis ex convallis justo iaculis dapibus sed sollicitudin leo. Nam ac mattis urna. Sed fermentum lacus sed ipsum gravida, quis egestas ligula vestibulum. Quisque suscipit, ex malesuada eleifend posuere, velit ex mollis nibh, id ultrices sapien ligula consectetur mi. Vestibulum ac tellus hendrerit, euismod lorem eget, tempus ipsum. Ut sed ex pharetra, volutpat ex et, iaculis dui. Duis varius odio ac quam tempor semper. Aenean nisi dolor, bibendum sed luctus ac, pretium vitae sem. Quisque feugiat imperdiet arcu, sit amet lacinia nunc tempus euismod</div>
 
+		<div class="col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque rhoncus metus a consectetur. Donec id ligula nulla. Vivamus mattis ex convallis justo iaculis dapibus sed sollicitudin leo. Nam ac mattis urna. Sed fermentum lacus sed ipsum gravida, quis egestas ligula vestibulum. Quisque suscipit, ex malesuada eleifend posuere, velit ex mollis nibh, id ultrices sapien ligula consectetur mi. Vestibulum ac tellus hendrerit, euismod lorem eget, tempus ipsum. Ut sed ex pharetra, volutpat ex et, iaculis dui. Duis varius odio ac quam tempor semper. Aenean nisi dolor, bibendum sed luctus ac, pretium vitae sem. Quisque feugiat imperdiet arcu, sit amet lacinia nunc tempus euismod</div>
+	</div>
+</div>
 
 	<script src="/js/script.js"></script>
   </body>
